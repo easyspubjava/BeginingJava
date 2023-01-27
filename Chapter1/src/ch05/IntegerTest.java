d7ce8b6423379d3ffe3da46fe009f2619b47a0ce
Park EunJong <parkeunjong@Parkui-iMac.local>
2023-01-27 15:15:07 +0900

    Chapter1

---
+++ Chapter1/src/ch05/IntegerTest.java
@@ -0,0 +1,18 @@
+package ch05;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+
+		byte bs = 127;
+		System.out.println(bs);
+		
+		//int iVal = 12345678900;
+		long lVal = 12345678900L;
+	
+		System.out.println(lVal);
+		
+
+	}
+
+}
