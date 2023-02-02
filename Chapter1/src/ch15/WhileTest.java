c9d149f1bbd28baa8bf0c7c5c7b6d48488062fe9
Park EunJong <parkeunjong@Parkui-MacBookPro-2.local>
2023-02-02 16:51:23 +0900

    Chapter1

---
+++ Chapter1/src/ch15/WhileTest.java
@@ -0,0 +1,22 @@
+package ch15;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+
+		int num=1;
+		int sum=0;
+		
+		while ( num<= 10 ) {
+			
+			sum += num;
+			num++;
+			
+		}
+		System.out.println(sum);
+		System.out.println(num);
+		System.out.println(num+1);
+		System.out.println(num);
+	}
+
+}
