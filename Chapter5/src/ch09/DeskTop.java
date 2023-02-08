d60b878037ee8bf34209fbc86269078f7b23b951
Park EunJong <parkeunjong@Parkui-iMac.local>
2023-02-09 08:46:48 +0900

    Chapter5

---
+++ Chapter5/src/ch09/DeskTop.java
@@ -0,0 +1,20 @@
+package ch09;
+
+public class DeskTop extends Computer{
+
+	@Override
+	public void display() {
+		System.out.println("DeskTop display");
+	}
+
+	@Override
+	public void typing() {
+		System.out.println("DeskTop typing");		
+	}
+	
+	@Override
+	public void turnOff() {
+		System.out.println("DeskTop turnOff");
+	}
+
+}
---
+++ Chapter5/src/ch09/MyNoteBook.java
@@ -0,0 +1,11 @@
+package ch09;
+
+public class MyNoteBook extends NoteBook{
+
+	@Override
+	public void display() {
+
+		System.out.println("MyNoteBook display");
+	}
+
+}
